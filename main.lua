2973fa4add783ab059fe415ed7e9aa0c62ada0aa
schazers <schazers@gmail.com>
2018-07-23 14:41:10 -0700

    removal video loading/rendering, just draw hello world

--- main.lua
+++ main.lua
@@ -1,17 +1,13 @@
 video = nil
 
 function love.load()
-	local vidStream = love.video.newVideoStream("video.ogg")
-	video = love.graphics.newVideo(vidStream)
-	video:seek(0)
-	video:play()
+	--local vidStream = love.video.newVideoStream("video.ogg")
+	--video = love.graphics.newVideo(vidStream)
+	--video:seek(0)
+	--video:play()
 end
 
 function love.draw()
 	love.graphics.print("Hello World!", 400, 300)
-	love.graphics.draw(video, 100, 100)
-end
-
-function love.update(dt)
-
+	--love.graphics.draw(video, 100, 100)
 end
